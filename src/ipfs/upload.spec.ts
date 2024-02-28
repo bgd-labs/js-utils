dbeb2c19c188b124faead60f8093a6dbac878f2e
Lukas <lukasstrassel@googlemail.com>
2024-02-28 09:13:26 +0100

    feat: add quicknode ipfs (#30)
    * feat: add quicknode ipfs
    
    * fix: don't inline api key

---
+++ src/ipfs/upload.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest';
+import { uploadToQuicknode } from './upload';
+
+describe('upload', () => {
+  it('upload to quicknode', async () => {
+    if (!process.env.QUICKNODE_API_KEY) return;
+    const response = await uploadToQuicknode('BoredGhosts');
+    console.log(response);
+  });
+});
